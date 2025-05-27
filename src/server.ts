61cbef89f4d836ad38e161e73d4c1d79c11f831d
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-05-27 14:50:27 -0500

    Add: morgan and dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import morgan from 'morgan'
 import taskRouter from './routes/taskRouter'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
 app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use('/api/v1/tasks', taskRouter)
 
