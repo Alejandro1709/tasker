5fa4d86d3aa490a82e71046dd7e55d375bbe6943
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-05-27 20:54:04 -0500

    Add: Auth routes and controller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express'
+import catchAsync from '../utils/catchAsync'
+
+export const login = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({ message: 'Ok' })
+  }
+)
+
+export const register = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({ message: 'Ok' })
+  }
+)
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import taskRouter from './routes/taskRouter'
+import authRouter from './routes/authRouter'
 import dotenv from 'dotenv'
 import { globalErrorHandler } from './middlewares/error'
 import connectDB from './config/db'
@@ -18,6 +19,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use('/api/v1/tasks', taskRouter)
+app.use('/api/v1/auth', authRouter)
 
 app.use((req, res, next) => {
   res.status(404).json({ message: 'This route does not exists' })
